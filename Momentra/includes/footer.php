261bbda66d22b49e75cead1a2bb7cdd17f04005a
rahmaezz06 <rahmaezzeldeen0@gmail.com>
2026-05-01 17:01:01 +0300

    Delete Momentra directory

--- Momentra/includes/footer.php
+++
@@ -1,3 +0,0 @@
-</body>
-</html>
-<?php if (ob_get_level()) ob_end_flush(); ?>
--- Momentra/pages/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . '/../includes/auth.php';
-logout();
