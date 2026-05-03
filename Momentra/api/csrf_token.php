d126f1a96919ec1aefbbeffa419cb3e2e411c4f6
rahmaezz06 <rahmaezzeldeen0@gmail.com>
2026-05-03 20:25:26 +0300

    Delete Momentra directory

--- Momentra/api/csrf_token.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . '/../includes/auth.php';
-header('Content-Type: application/json');
-startSession();
-echo json_encode(['csrf_token' => csrfToken()]);
